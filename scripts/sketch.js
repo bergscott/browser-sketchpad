2551f73220043f3e8878f7a146efaafa83be08b2
Scott Berg <berg.scott@ymail.com>
2016-02-17 18:25:23 -0600

    implemented resizing procedure

--- scripts/sketch.js
+++ scripts/sketch.js
@@ -1,13 +1,20 @@
 $(document).ready(function() {
-    var divrow = makeRow(16);
-    buildSketchpad(divrow, 16);
+    buildSketchpad_v2(16);
+    mouseListen();
+    $('#reset').click(function() {
+        $('#sketchpad').empty();
+        var user = +prompt('Generating new sketchpad.\nHow many boxes per side?');
+        setSize(user);
+        buildSketchpad_v2(user);
+        mouseListen();
+    });
+});
+
+var mouseListen = function() {
     $('#sketchpad .tile').mouseenter(function() {
 	$(this).addClass('active');
     });
-    $('#reset').click(function() {
-	$('#sketchpad .active').removeClass('active');
-    });
-});
+};
 
 var makeRow = function(colCount) {
     var result = '';
@@ -23,4 +30,26 @@ var buildSketchpad = function(row, rowCount) {
 	$fragment.append(row);
     }
     $('#sketchpad').append($fragment);
+};
+
+var addRows = function(row, rowCount) {
+    var $fragment = $(document.createDocumentFragment());
+    for (var i = 0; i < rowCount; i++) {
+	$fragment.append(row);
+    }
+    $('#sketchpad').append($fragment);
+};
+var buildSketchpad_v2 = function(size) {
+    var divrow = makeRow(size);
+    addRows(divrow, size);
+};
+
+var setSize = function(size) {
+    var $style = $('style');
+    var dimension = 800 / size;
+    dimension += 'px';
+    var selector = '#sketchpad .row .tile';
+    var attributes = '{width: '+dimension+'; height: '+dimension+';}';
+    $style.empty();
+    $style.append(selector + ' ' + attributes);
 };
