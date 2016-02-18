ee1aa94bf072a36a3b8961615f716051db2e6efd
bergscott <berg.scott@ymail.com>
2016-02-18 15:32:24 -0600

    rearranged script, replaced magic numbers with constants

--- scripts/sketch.js
+++ scripts/sketch.js
@@ -1,71 +1,30 @@
+var MAXBLUE = 128;
+var TOBLACK = 10;
+var BLACKADD = Math.ceil(MAXBLUE / TOBLACK);
+var INITSIZE = 16;
+var PADPIXELS = 800;
+
 $(document).ready(function() {
-    buildSketchpad(16);
-    mouseListen_v2();
+    var padSize = INITSIZE;
+    buildSketchpad(padSize);
+    mouseListen();
     $('#reset').click(function() {
         var user = +prompt(
 	    'Generating new sketchpad.\nHow many boxes per side?',
-	    '16');
+	    padSize);
 	if (user > 0) {
 	    $('#sketchpad').empty();
-	    setSize(user);
-	    buildSketchpad(user);
-	    mouseListen_v2();
+	    padSize = user;
+	    setSize(padSize);
+	    buildSketchpad(padSize);
+	    mouseListen();
 	}
     });
 });
 
-var mouseListen = function() {
-    $('#sketchpad .tile').mouseenter(function() {
-	$(this).addClass('active');
-    });
-};
-
-var mouseListen_v2 = function() {
-    $('#sketchpad .tile').mouseenter(function() {
-	if (document.getElementById('crazy').checked) {
-	    $(this).css({'background-color': getRandomColor()});
-	    $(this).addClass('active');
-	} else if ($(this).hasClass('active')) {
-	    var newColor = getDarkerColor($(this).css('background-color'));
-	    $(this).css({'background-color': newColor});
-	} else {
-	    $(this).addClass('active');
-	}
-    });
-};
-
-var getRandomColor = function() {
-    var get256 = function() {
-	return Math.floor(Math.random()*256);
-    };
-    var red = get256();
-    var blue = get256();
-    var green = get256();
-    return 'rgb('+red+','+blue+','+green+')';
-};
-
-var getDarkerColor = function(color) {
-    var parseRGB_blue = function(color) {
-	var result = [];
-	var result_index = 0;
-	var sub_start = 4;
-	for (var i = 4; i < color.length; i++) {
-	    if (color[i] === ',' || color[i] === ')') {
-		result[result_index] = color.substring(sub_start, i);
-		sub_start = i+1;
-		result_index++;
-	    }
-	}
-	return result[2];
-    }
-    var blue = +parseRGB_blue(color);
-    blue -= 13;
-    if (blue < 0) {
-	blue = 0;
-    } else if (blue > 128) {
-	blue = 128;
-    }
-    return 'rgb(0,0,'+blue+')';
+var buildSketchpad = function(size) {
+    var divrow = makeRow(size);
+    addRows(divrow, size);
 };
 
 var makeRow = function(colCount) {
@@ -83,20 +42,66 @@ var addRows = function(row, rowCount) {
     }
     $('#sketchpad').append($fragment);
 };
-var buildSketchpad = function(size) {
-    var divrow = makeRow(size);
-    addRows(divrow, size);
-};
 
 var setSize = function(size) {
     var $style = $('style');
-    var dimension = 800 / size;
-    if (dimension * size > 800) {
-	dimension -= (dimension * size - 800) / size;
-    }
+    var dimension = PADPIXELS / size;
     dimension += 'px';
     var selector = '#sketchpad .row .tile';
     var attributes = '{width: '+dimension+'; height: '+dimension+';}';
     $style.empty();
     $style.append(selector + ' ' + attributes);
 };
+
+var mouseListen = function() {
+    $('#sketchpad .tile').mouseenter(function() {
+	if (document.getElementById('crazy').checked) {
+	    $(this).css({'background-color': getRandomColor()});
+	    $(this).addClass('active');
+	} else if ($(this).hasClass('active')) {
+	    var newColor = getDarkerColor($(this).css('background-color'));
+	    $(this).css({'background-color': newColor});
+	} else {
+	    $(this).addClass('active');
+	}
+    });
+};
+
+var getRandomColor = function() {
+    var randRGBVal = function() {
+	return Math.floor(Math.random()*256);
+    };
+    return 'rgb('+randRGBVal()+','+randRGBVal()+','+randRGBVal()+')';
+};
+
+var getDarkerColor = function(color) {
+    var blue = +parseRGB(color)[2];
+    blue -= BLACKADD;
+    if (blue < 0) {
+	blue = 0;
+    } else if (blue > MAXBLUE) {
+	blue = MAXBLUE;
+    }
+    return 'rgb(0,0,'+blue+')';
+};
+
+// string -> array of 3 strings
+// parseRGB takes a string 'color' in the format of a CSS RGB color value
+// and returns an array of strings corresoponding to 
+// [redValue, greenValue, blueValue]
+// example: parseRGB("rgb(0,30,255)") -> ["0", "30", "255"]
+var parseRGB = function(color) {
+    var result = [];
+    var result_index = 0;
+    // loop starts at 4 to discard "rgb(" from start of color string
+    var sub_start = 4;
+    for (var i = 4; i < color.length; i++) {
+	if (color[i] === ',' || color[i] === ')') {
+	    result[result_index] = color.substring(sub_start, i);
+	    sub_start = i+1;
+	    result_index++;
+	}
+    }
+    return result;
+};
+
