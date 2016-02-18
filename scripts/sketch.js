edcbc02f94fb6b9827b6f5b59b496cdb9109d9b3
bergscott <berg.scott@ymail.com>
2016-02-18 13:33:27 -0600

    implemented "crazy colors" mode and gradual darkening for "standard mode"

--- scripts/sketch.js
+++ scripts/sketch.js
@@ -1,13 +1,13 @@
 $(document).ready(function() {
     buildSketchpad(16);
-    mouseListen();
+    mouseListen_v2();
     $('#reset').click(function() {
         var user = +prompt('Generating new sketchpad.\nHow many boxes per side?');
 	if (user > 0) {
 	    $('#sketchpad').empty();
 	    setSize(user);
 	    buildSketchpad(user);
-	    mouseListen();
+	    mouseListen_v2();
 	}
     });
 });
@@ -16,6 +16,54 @@ var mouseListen = function() {
     $('#sketchpad .tile').mouseenter(function() {
 	$(this).addClass('active');
     });
+};
+
+var mouseListen_v2 = function() {
+    $('#sketchpad .tile').mouseenter(function() {
+	if (document.getElementById('crazy').checked) {
+	    $(this).css({'background-color': getRandomColor()});
+	    $(this).addClass('active');
+	} else if ($(this).hasClass('active')) {
+	    var newColor = getDarkerColor($(this).css('background-color'));
+	    $(this).css({'background-color': newColor});
+	} else {
+	    $(this).addClass('active');
+	}
+    });
+};
+
+var getRandomColor = function() {
+    var get256 = function() {
+	return Math.floor(Math.random()*256);
+    };
+    var red = get256();
+    var blue = get256();
+    var green = get256();
+    return 'rgb('+red+','+blue+','+green+')';
+};
+
+var getDarkerColor = function(color) {
+    var parseRGB_blue = function(color) {
+	var result = [];
+	var result_index = 0;
+	var sub_start = 4;
+	for (var i = 4; i < color.length; i++) {
+	    if (color[i] === ',' || color[i] === ')') {
+		result[result_index] = color.substring(sub_start, i);
+		sub_start = i+1;
+		result_index++;
+	    }
+	}
+	return result[2];
+    }
+    var blue = +parseRGB_blue(color);
+    blue -= 13;
+    if (blue < 0) {
+	blue = 0;
+    } else if (blue > 128) {
+	blue = 128;
+    }
+    return 'rgb(0,0,'+blue+')';
 };
 
 var makeRow = function(colCount) {
